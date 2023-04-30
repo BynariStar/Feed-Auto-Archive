1b77dc721041d6cb109d70067355f387affe0307
moatt <moattasam@gmail.com>
2023-04-30 13:30:20 +0300

    added proxy support, separate configs for feeds group, checking if url was already archived

--- config/config.js
+++ config/config.js
@@ -1,15 +1,19 @@
 let config = {};
 
 // Feed request interval in seconds, defaults to 60 if not defined
-config.interval = 60;
-
-// Comment out to use port provided by the Node environment
-//config.port = 8080;
+config.interval = 600;
 
 // Keeps the query (everything after the '?') from the url, defaults to false
 config.keepQuery = false;
 
 // Archives feeds to Archive.org by default
-config.archiveEndpoint = "https://web.archive.org/save/";
+config.archiveSaveEndpoint = "https://web.archive.org/save/";
+config.archiveAvailableEndpoint = "https://archive.org/wayback/available?url=";
+
+config.proxy = "http://localhost:8123";
+
+config.checkSavedBefore = true;
+
+config.tooManyRequestsSleepTime = 60;
 
 module.exports = config;
